4bd9d7db238aaf437c0f86cce0c3d2a385cb145e
Atul-Sharma-1430 <atulsharma000007@gmail.com>
2026-05-02 13:58:29 +0530

    Doubt Clearing

--- JS/Hoisting/Examples.js
+++ JS/Hoisting/Examples.js
@@ -1,36 +1,36 @@
 
 // var hoisting
-console.log(a); // undefined (declaration hoisted)
+console.log("Line 3", a); // undefined (declaration hoisted)
 var a = 10;
-console.log(a); // 10
+console.log("Line 5", a); // 10
 
 
 
 // let hoisting (TDZ)
 try {
-    console.log(b); // ReferenceError
+    console.log("Line 11", b); // ReferenceError --> Will Not be Printed
 } catch (e) {
-    console.log("let hoisting error");
+    console.log("Line 13 let hoisting error");
 }
 let b = 20;
-console.log(b); // 20
+console.log("Line 16", b); // 20
 
 
 // const hoisting (TDZ)
 try {
-    console.log(c); // ReferenceError
+    console.log("Line 21", c); // ReferenceError --> Will Not be Printed
 } catch (e) {
-    console.log("const hoisting error");
+    console.log("Line 23 const hoisting error");
 }
 const c = 30;
-console.log(c); // 30
+console.log("Line 26", c); // 30
 
 
 
 // Function declaration hoisting
 sayHello(); // works
 function sayHello() {
-    console.log("Hello from function declaration");
+    console.log("Line 33 Hello from function declaration");
 }
 
 
@@ -38,11 +38,11 @@ function sayHello() {
 try {
     sayHi(); // TypeError as sayHi is the variable in whichi it is stores  it is not function
 } catch (e) {
-    console.log("function expression error");
+    console.log("Line 41 function expression error");
 }
 
 var sayHi = function () {
-    console.log("Hi from function expression");
+    console.log("Line 45 Hi from function expression");
 };
 sayHi(); // works now
 
@@ -52,11 +52,11 @@ sayHi(); // works now
 try {
     sayArrow(); // TypeError
 } catch (e) {
-    console.log("arrow function error");
+    console.log("Line 55 arrow function error");
 }
 
 const sayArrow = () => {
-    console.log("Hello from arrow function");
+    console.log("Line 59 Hello from arrow function");
 };
 sayArrow(); // works now
 
